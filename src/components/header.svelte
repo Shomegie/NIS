c0add7ecbfee7f3496676b52d9de24d0c5430618
Shomegie <shomegie@gmail.com>
2022-07-31 09:54:48 +0100

    Setup carousel with the help of the Splide package

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -7,7 +7,7 @@
 <section>
     <div class="h-16 w-full bg-amber-400 flex items-center">
         <nav class="flex w-4/5 mx-auto justify-center ">
-            <div class="text-yellow-50 text-md font-bold flex justify-center pr-8">Nithalos International School</div>
+            <div class="text-yellow-50 text-md font-bold flex justify-center pr-8">NIS</div>
             <div class="justify-center flex">
                 <ul class="text-amber-50 font-bold flex space-x-6">
                     <li class={li}>Home</li>
--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,1 +1,29 @@
-Home Page
+<script>
+    import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+    import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
+    let images = `object-contain`
+</script>
+    
+    <Splide class="w-full"  options={ { rewind: true } } aria-label="My Favorite Images">
+      <!-- <SplideSlide>
+        <div class="h-96 w-full bg-[url('mage1.jpg')] bg-cover bg-no-repeat"></div>
+      </SplideSlide>
+      <SplideSlide>
+        <div class="h-96 w-full bg-[url('mage2.jpg')] bg-cover bg-no-repeat bg-center"></div>
+      </SplideSlide>
+      <SplideSlide>
+        <div class="h-96 w-full bg-[url('mage3.jpg')] bg-cover bg-no-repeat bg-bottom"></div>
+      </SplideSlide> -->
+
+      <SplideSlide>
+        <div class="h-[480px] w-full bg-teal-500"></div>
+      </SplideSlide>
+      <SplideSlide>
+        <div class="h-[480px] w-full bg-orange-500"></div>
+      </SplideSlide>
+      <SplideSlide>
+        <div class="h-[480px] w-full bg-pink-500"></div>
+      </SplideSlide>
+
+    </Splide>
